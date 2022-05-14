ae0504855cf7e91efda3f7e545a33667fbb36321
Timi Alabi <alabitimi65@gmail.com>
2022-05-14 15:33:02 +0000

    icecream.js

---
+++ loopy-lighthouse.js
@@ -0,0 +1,20 @@
+var n = 100;
+while (n <= 200) {
+  if (n % 3 === 0) {
+    console.log("Loopy");
+  } else if (n % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log("LoopyLighthouse");
+  }
+  n++;
+}
+
+/*console.log(n);
+
+function sayHello() {
+  var message = "Hello!";
+  return message;
+}
+
+console.log(sayHello());*/
